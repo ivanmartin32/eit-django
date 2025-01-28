e4586959c9d717ef215c02078ee043f65a89929c
ivanmartin32 <ivanmartin_32@hotmail.com>
2025-01-28 16:40:22 -0300

    Ultima modificacion

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from .models import Cliente, Pais
 
 admin.site.register(Pais)
-admin.site.register(Cliente)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido','nacimiento','pais_origen')
+    search_fields = ('nombre','apellido')
+    list_filter = ('pais_origen',)
 # Register your models here.
